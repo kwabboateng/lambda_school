bd9ccf65d58590abbcce69b92ac439c32ddba2ef
Kwabena Boateng <kwab.boateng@gmail.com>
2020-05-07 12:28:48 -0400

    polish examples

--- lambda_school_oop.py
+++ lambda_school_oop.py
@@ -18,10 +18,21 @@ class Employee:
         self.salary = salary
         self.department = None
     def income_tax(self):
-        return self.salary*(0.22)
+        if self.salary <= 9700:
+            return self.salary*(0.10)
+        elif self.salary <= 39475:
+            return self.salary*(0.12)
+        elif self.salary <= 84200:
+            return self.salary*(0.22)
+    def hourly_rate(self):
+        return self.salary / (40*50)
+    def __str__(self):
+        return "{ name: " + self.name + ", " + "salary: " + str(self.salary) + ", " + "department: " + str(self.department) + " }"
 
 me = Employee("Kwabena", 50000)
 print(me)
+print(me.name)
+print(me.salary)
 
 
 #################
